44d06d5a754c7b8ae0e01cb4330345a311ef185b
neyadrian <neyadrian2018@gmail.com>
2026-08-01 14:06:40 -0300

    substituindo os construtores pela annotation AUTORIWED

--- aula/src/main/java/com/devsuperior/services/SalaryService.java
+++ aula/src/main/java/com/devsuperior/services/SalaryService.java
@@ -1,18 +1,17 @@
 package com.devsuperior.services;
 
 import com.devsuperior.entities.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SalaryService {
 
+    @Autowired
     private TaxService taxService;
+    @Autowired
     private PensionService pensionService;
 
-    public SalaryService(TaxService taxService, PensionService pensionService) {
-        this.taxService = taxService;
-        this.pensionService = pensionService;
-    }
 
     public double netSalary(Employee employee) {
         return employee.getGrossSalary() - taxService.tax(employee.getGrossSalary())
